135c0341ac9e8a8399a51bf00d73eaf7ecb93741
ajaythakur-aps <ajaypr.singh056@gmail.com>
2025-11-01 11:59:46 +0530

    9th June All Files

---
+++ src/Lec002/June9_Lecture.java
@@ -0,0 +1,17 @@
+package Lec002;
+
+public class June9_Lecture {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        // This is the single line Comment
+        //The single line comment write bye //
+        System.out.println("Hi");
+        System.out.println("Bye");
+        System.out.println("Drink");
+        System.out.println("Chai");
+
+        /* This is the multiple line comment
+            And the multiple line Comment Write in the
+        */
+    }
+}
---
+++ src/Lec002/_June9th_Lecture1.java
@@ -0,0 +1,13 @@
+package Lec002;
+
+public class _June9th_Lecture1 {
+    static void main() {
+        System.out.println("_June9th_Lecture this class is possible");
+
+        /* Class should be start With Dollar($),underScore(_) and Any alphabetical
+        name can not start with number
+         */
+
+        System.out.println("Ths is the naming convention");
+    }
+}
